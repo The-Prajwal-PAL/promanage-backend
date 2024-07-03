94c068e5c70919d248d8b3510e6bcb9944052cc0
Prajwal Pal <61036811+Prajwalpal10@users.noreply.github.com>
2024-07-03 18:56:23 +0530

    user email

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import dotenv from "dotenv";
 dotenv.config();
 import { app } from "./app.js";
 import connectDB from "./db/index.js";
+import { User } from "./models/user.models.js";
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT, () => {
+    app.listen(4000, () => {
       console.log("app is runnign at the above");
-      console.log(process.env.PORT);
     });
   })
   .catch((er) => {
@@ -17,4 +17,30 @@ app.get("/", (req, res) => {
   res.status(200).json({
     message: "hello there",
   });
+
+  // app.get('/all', async (req, res) => {
+  //   try {
+  //     const users = await User.find({}, 'email'); // Retrieve only the email field
+  //     const userInfos = users.map(user => ({ id: user._id, email: user.email }));
+  //     res.json(userInfos);
+  //   } catch (err) {
+  //     res.status(500).send('Server error');
+  //   }
+  // });
+  
+  app.get('/api/users',async (req, res) => {
+    try {
+      // Simulate delay to mock API response time (remove in production)
+      
+        const users = await User.find({}, 'email'); // Retrieve only the email field
+     const userInfos = users.map(user => ({ id: user._id, email: user.email }));
+     res.json(userInfos);
+      }// 1 second delay
+     catch (error) {
+      console.error('Error fetching users:', error);
+      res.status(500).json({ error: 'Failed to fetch users' });
+    }
+  });
+
+ 
 });
